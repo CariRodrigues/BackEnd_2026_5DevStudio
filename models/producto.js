a100544abe3d878d07ca2061d27efb293f6a4d27
miriqm <arialtek@gmail.com>
2026-05-23 00:23:44 -0300

    update branch

--- models/producto.js
+++
@@ -1,9 +0,0 @@
-class Producto {
-  constructor(id, nombre, precio, stock) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.stock = stock;
-  }
-}
-module.exports = Producto;

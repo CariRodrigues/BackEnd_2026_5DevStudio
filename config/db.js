0af40a376cec62cf93a1d00ad952562d5b8d143f
miriqm <120044756+miriqm@users.noreply.github.com>
2026-05-24 15:37:41 -0300

    se agrega funcion para crear usuario admin y user como default

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+import crearDefaultUsers from "./crearDefaultUsers.js";
 
 export const conectarDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB conectado");
+
+    // Crear usuarios por defecto
+    await crearDefaultUsers();
+
   } catch (error) {
     console.error(error);
     process.exit(1);

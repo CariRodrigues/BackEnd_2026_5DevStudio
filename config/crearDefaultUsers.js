785745a4d2384493af5f018a2b3227bc10a4217f
Carina Rodrigues <crodrigues.prog@gmail.com>
2026-06-24 20:20:07 -0300

    modificación script en crearDefaultUsers.js

--- config/crearDefaultUsers.js
+++ config/crearDefaultUsers.js
@@ -5,32 +5,42 @@ async function crearDefaultUsers() {
 
   try {
     const admin = await Usuario.findOne({
-      username: "admin",
+      email: "admin@todostock.com",
     });
 
     console.log("Admin encontrado:", admin);
 
     if (!admin) {
+      const { salt, passwordHash } =
+        Usuario.crearPasswordSeguro("1234");
+
       await Usuario.create({
-        username: "admin",
-        password: "1234",
-        rol: "admin",
+        nombre: "Administrador",
+        email: "admin@todostock.com",
+        passwordHash,
+        salt,
+        rol: "admin"
       });
 
       console.log("Admin creado");
     }
 
     const user = await Usuario.findOne({
-      username: "user",
+      email: "user@todostock.com",
     });
 
     console.log("User encontrado:", user);
 
     if (!user) {
+      const { salt, passwordHash } =
+        Usuario.crearPasswordSeguro("1234");
+
       await Usuario.create({
-        username: "user",
-        password: "1234",
-        rol: "user",
+        nombre: "Usuario",
+        email: "user@todostock.com",
+        passwordHash,
+        salt,
+        rol: "user"
       });
 
       console.log("Usuario creado");
